e213bf15136c8be51e53fe405ec6327dc74d559c
olkinin <olkinin@mail.ru>
2022-11-05 09:11:49 +0200

    commit

--- src/main/java/ru/geekbrains/autumnmarket/products/Product.java
+++ src/main/java/ru/geekbrains/autumnmarket/products/Product.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 
 public class Product {
     @Id
-    @GeneratedValue
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
     @Column(name = "title")
